ecf7fb3fb01b892b7be08fdd7727aa0f3dd69a5a
lalitjoshi06 <ljoshi49@gmail.com>
2024-03-27 11:35:59 +0530

    remove

--- pages/api/channelfilter.js
+++
@@ -1,7 +0,0 @@
-const skipChannelIds = [632,840,956];
-
-const filterChannels = (channels) => {
-    return channels.filter(channel => !skipChannelIds.includes(channel.channelMeta.id));
-};
-
-module.exports = { filterChannels };
